822605e17b27820dfb05d0f2ba241239ebee0490
githubowy-juliusz <githubowy-juliusz@wp.pl>
2020-12-24 09:46:16 +0100

    Added difficulty levels

--- app/src/main/java/lab/main/puzzle/PuzzleFragment.kt
+++ app/src/main/java/lab/main/puzzle/PuzzleFragment.kt
@@ -2,21 +2,32 @@ package lab.main.puzzle
 
 import android.os.Bundle
 import android.os.SystemClock
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.Chronometer
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import lab.main.R
 
 class PuzzleFragment : Fragment(R.layout.puzzle_fragment) {
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
+		val layout = view.findViewById<ConstraintLayout>(R.id.puzzleLayout)
 		val timer = view.findViewById<Chronometer>(R.id.puzzleTimer)
 		val restartButton = view.findViewById<Button>(R.id.puzzleRestartButton)
 		val touchLayer = view.findViewById<ImageView>(R.id.puzzleTouchLayer)
 		val movesText = view.findViewById<TextView>(R.id.puzzleMovesText)
+		val difficultyLayout =
+			view.findViewById<ConstraintLayout>(R.id.puzzleDifficultyLayout)
+		val difficultyButtonUnlimited =
+			view.findViewById<Button>(R.id.puzzleDifficultyButtonUnlimited)
+		val difficultyButton10Min =
+			view.findViewById<Button>(R.id.puzzleDifficultyButton10Min)
+		val difficultyButton1Min =
+			view.findViewById<Button>(R.id.puzzleDifficultyButton1Min)
 		val positions = listOf(
 			view.findViewById<ImageView>(R.id.puzzlePosition1),
 			view.findViewById<ImageView>(R.id.puzzlePosition2),
@@ -29,7 +40,7 @@ class PuzzleFragment : Fragment(R.layout.puzzle_fragment) {
 			view.findViewById<ImageView>(R.id.puzzlePosition9)
 		)
 		val pictureSetter = PictureSetter()
-
+		var timeLimit = 0
 		fun updatePositions(game: PuzzleGame) {
 			if(game.isSolved) {
 				timer.stop()
@@ -46,9 +57,7 @@ class PuzzleFragment : Fragment(R.layout.puzzle_fragment) {
 			}
 		}
 
-		restartButton.text = "Start"
-		restartButton.setOnClickListener {
-			restartButton.text = "Restart"
+		fun startGame() {
 			val game = PuzzleGame()
 			updatePositions(game)
 			touchLayer.setOnTouchListener(
@@ -58,6 +67,40 @@ class PuzzleFragment : Fragment(R.layout.puzzle_fragment) {
 			)
 			timer.base = SystemClock.elapsedRealtime()
 			timer.start()
+		}
+
+		restartButton.text = "Start"
+		restartButton.setOnClickListener {
+			restartButton.text = "Restart"
+			difficultyLayout.visibility = View.VISIBLE
+		}
+
+		timer.setOnChronometerTickListener {
+			Log.d("Hello", "Is this working as intended?")
+			if(timeLimit == 0)
+				return@setOnChronometerTickListener
+			val elapsedTime = (SystemClock.elapsedRealtime() - timer.base) / 1000
+			if(elapsedTime >= timeLimit) {
+				timer.stop()
+				movesText.text = "You have run out of time."
+				touchLayer.setOnTouchListener(null)
+			}
+		}
+
+		difficultyButtonUnlimited.setOnClickListener {
+			timeLimit = 0
+			startGame()
+			difficultyLayout.visibility = View.GONE
+		}
+		difficultyButton1Min.setOnClickListener {
+			timeLimit = 60
+			startGame()
+			difficultyLayout.visibility = View.GONE
+		}
+		difficultyButton10Min.setOnClickListener {
+			timeLimit = 60 * 10
+			startGame()
+			difficultyLayout.visibility = View.GONE
 		}
 	}
 }
